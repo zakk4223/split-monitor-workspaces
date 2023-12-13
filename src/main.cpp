ebee56e0a87d08bd02ef84ac9a842f0d455aa931
Zakk <zakk@rsdio.com>
2023-12-13 03:38:56 -0500

    Don't issue useless hyprctl command

--- src/main.cpp
+++ src/main.cpp
@@ -134,7 +134,7 @@ void mapWorkspacesToMonitors()
         for (int i = workspaceIndex; i < workspaceIndex + workspaceCount; i++) {
             std::string workspaceName = std::to_string(i);
             g_vMonitorWorkspaceMap[monitor->ID].push_back(workspaceName);
-            HyprlandAPI::invokeHyprctlCommand("keyword", "workspace " + workspaceName + "," + monitor->szName);
+            //HyprlandAPI::invokeHyprctlCommand("keyword", "workspace " + workspaceName + "," + monitor->szName);
             CWorkspace* workspace = g_pCompositor->getWorkspaceByName(workspaceName);
 
             if (workspace != nullptr) {
